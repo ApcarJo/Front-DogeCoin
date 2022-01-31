17d61cd1037ed94c5fd7b52ab5e3c45d5908781c
José Luis <hansokaz@gmail.com>
2022-01-31 11:58:53 +0100

    Added logout button on transactions

--- src/containers/Transactions/Transactions.jsx
+++ src/containers/Transactions/Transactions.jsx
@@ -5,6 +5,7 @@ import moment from 'moment';
 import spinner from '../../img/spinner2.gif'
 import TransactionBox from '../../components/TransactionBox/TransactionBox'
 import { useNavigate } from 'react-router-dom';
+import { LOGOUT } from '../../redux/types';
 
 const Transaction = (props) => {
 
@@ -18,7 +19,12 @@ const Transaction = (props) => {
     }, []);
 
     const newDate = new Date();
-    const transArray = [];
+
+    const logOut = () => {
+
+        props.dispatch({type:LOGOUT});
+        navigate("/");
+    }
 
     const getTransactions = async () => {
         try {
@@ -31,9 +37,6 @@ const Transaction = (props) => {
             // let res = await axios.post(`https://dogecoin-back.herokuapp.com/transactions/all`, body, { headers: { 'authorization': 'Bearer ' + token } });
             let res = await axios.post('https://dogecoin-back.herokuapp.com/user/getUser', body, { headers: { 'authorization': 'Bearer ' + token } });
             setTransactions(res.data.transactions);
-            // for (let i in transactions) {
-            //     transArray.push(i);
-            // }
 
         } catch (err) {
             console.log({ message: err.message });
@@ -42,42 +45,45 @@ const Transaction = (props) => {
     if (transactions) {
         return (
             <div className="center column">
-                <div className="bg-grey">
+                <div className="boxTr bg-grey">
                     <div className="headerTr">
-                    <span onClick={()=>navigate(-1)}>back</span>
-                    <p>Dogecoin</p>
-                    <p>tools</p>
-                </div>
-                <div>
-                    {transactions.map((a, index) => (
-                        <div className="">
-                            <div>
-                                <span className="dateTr df left">
-                                    {(moment(props.date).format('DDMMYY') == moment(newDate).format('DDMMYY')) ? moment(a.Date).format("MMM Do YY") : null}
-                                </span>
+                        <span className="navigateButtons" onClick={() => navigate(-1)}>back</span>
+                        <span>Dogecoin</span>
+                        <span className="navigateButtons" onClick={()=>logOut()}>LogOut</span>
+                    </div>
+                    <div className="center paddingY">
+                    <button className="sendTr" onClick={()=>navigate('/newTransaction')}> Send </button>
+                    </div>
+                    <div>
+                        {transactions.map((a, index) => (
+                            <div className="">
+                                <div>
+                                    <span className="dateTr df left">
+                                        {(moment(props.date).format('DDMMYY') == moment(newDate).format('DDMMYY')) ? moment(a.Date).format("MMM Do YY") : null}
+                                    </span>
+                                </div>
+                                <TransactionBox
+                                    address={a.address}
+                                    quantity={a.quantity}
+                                    isSent={a.isSent}
+                                    date={a.date}
+                                />
                             </div>
-                            <TransactionBox
-                                address={a.address}
-                                quantity={a.quantity}
-                                isSent={a.isSent}
-                                date={a.date}
-                            />
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
             </div >
         );
     } else {
 
-    return (
-        <div className="spinnerContainer">
-            <div className="spinner">
-                <img src={spinner} alt="spinner" width="60" />
+        return (
+            <div className="spinnerContainer">
+                <div className="spinner">
+                    <img src={spinner} alt="spinner" width="60" />
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 }
 
 export default connect((state) => ({
