740e9cc6007a6468d8ba1fbdea0267f0cd59cd2e
José Luis <hansokaz@gmail.com>
2022-01-31 05:17:07 +0100

    Added login view

---
+++ src/containers/Login/Login.jsx
@@ -0,0 +1,96 @@
+
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+// import Link from '../../components/Link/Link'
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { LOGIN } from '../../redux/types';
+
+const Login = (props) => {
+
+    let navigate = useNavigate();
+
+    const [credentials, setCredentials] = useState({ email: '', password: '' });
+    const [msgError, setMensajeError] = useState({ eEmail: '', ePassword: '', eValidate: '' });
+
+    const updateCredentials = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    }
+
+    useEffect(() => {
+    }, []);
+
+    useEffect(() => {
+    });
+
+    const checkError = async (arg) => {
+
+        switch (arg) {
+
+            case 'email':
+
+                if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(credentials.email)) {
+                    setMensajeError({ ...msgError, eEmail: "Please, enter your email" });
+                } else {
+                    setMensajeError({ ...msgError, eEmail: "" });
+                }
+                break;
+
+            case 'password':
+
+                if (credentials.password.length < 1) {
+                    setMensajeError({ ...msgError, ePassword: "Please, enter your password" });
+                } else {
+                    setMensajeError({ ...msgError, ePassword: "" });
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    const logeame = async () => {
+        try {
+            let body = {
+                email: credentials.email,
+                password: credentials.password
+            }
+            // Envío por axios
+            let res = await axios.post(`https://dogecoin-back.herokuapp.com/login`, body);
+
+            props.dispatch({ type: LOGIN, payload: res.data });
+
+            // redirección
+            setTimeout(() => {
+                navigate('transactions');
+            }, 250);
+
+        } catch {
+            setMensajeError({ ...msgError, eValidate: 'Wrong email or password' });
+        }
+    }
+
+    return (
+        <div className="vistaLogin">
+            <div className="actionCard center column">
+                LOGIN
+                <input className="loginBox" name="email" type="text" onChange={updateCredentials} onBlur={() => checkError("mail")} placeholder="email" required />
+                <div className="errorsText">{msgError.eEmail}</div>
+
+                <input className="loginBox" name="password" type="password" onChange={updateCredentials} onBlur={() => checkError("password")} placeholder="password" required />
+                <div className="errorsText">{msgError.ePassword}</div>
+                <br></br>
+                <div className="actionButtons df row">
+                    <div className="sendButton center" onClick={() => logeame()}>Sign in</div>
+                    <div>{msgError.eValidate}</div>
+                    <div className="sendButton" onClick={() => navigate(`/register`)}>Register now!</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+// export default connect()(Login);
+export default connect((state)=> ({
+    credentials: state.credentials
+}))(Login);
